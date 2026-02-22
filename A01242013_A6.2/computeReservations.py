559a1221167c9b19eb72c1e3bc1d1395ef3a0aff
Michelle Alicia Graver Aguilar <A01242013@tec.mx>
2026-02-22 08:21:32 -0600

    feat: New StandardHotel class has been added

--- A01242013_A6.2/computeReservations.py
+++ A01242013_A6.2/computeReservations.py
@@ -5,12 +5,22 @@ from abc import ABC, abstractmethod
 
 "Program that manages Hotels and their inherent activities"
 
-
+#Hotel Classes 
 class Hotel(ABC):
 
     @abstractmethod
-    def to_dictionar(self):
+    def to_dictionary(self):
         pass
 
+
+class StandardHotel(Hotel):
+
+    def __init__(self, hotel_id, name):
+        self.hotel_id = hotel_id
+        self.name = name 
+
+    def to_dictionary(self):
+        return self.__dict__
+    
 
 
