6d60ce8b1ac9c630620509a294f216a9ba29cf6f
Michelle Alicia Graver Aguilar <A01242013@tec.mx>
2026-02-22 11:58:13 -0600

    fix: Fixes done to the program to comply with pylint

--- A01242013_A6.2/compute_reservations.py
+++ A01242013_A6.2/compute_reservations.py
@@ -1,171 +1,180 @@
+"""Program that manages Hotels and their inherent activities"""
+
 import argparse
 from abc import ABC, abstractmethod
 import json
 
-#pylint: disable=invalid-name
+#pylint: disable=too-few-public-methods
 
-"Program that manages Hotels and their inherent activities"
 
-def load_file(file):
-    with open(input_file.file, "r", encoding="utf-8") as f:
+def load_file(file_path):
+    """Function to load a file, used in multiple classes"""
+    with open(file_path, "r", encoding="utf-8") as f:
         file_dict = json.load(f)
-    
+
     return file_dict
 
-def save_file(d):
-    with open(input_file.file, "w", encoding="utf-8") as f:
+def save_file(file_path, d):
+    """Function to save a file, used in multiple classes"""
+    with open(file_path, "w", encoding="utf-8") as f:
         json.dump(d, f, indent=4)
 
 
-#Hotel Classes 
+#Hotel Classes
 class Hotel(ABC):
-
+    """Abstract class for hotels"""
     @abstractmethod
     def to_dictionary(self):
-        pass
+        """Function to return its own dictionary"""
 
 
 class StandardHotel(Hotel):
-
+    """Class that handles the methods needed for a standard hotel"""
     def __init__(self, hotel_id, name):
         self.hotel_id = hotel_id
-        self.name = name 
+        self.name = name
 
     def to_dictionary(self):
         return self.__dict__
-    
+
 
 class ManageHotel:
-
+    """Class that handles the methods to manage the hotels data"""
     def __init__(self, f):
-        self.FILE = f
+        self.file = f
 
-    def create_hotel(self, id, name):
-        new_hotel = {'hotel_id': id, 'hotel_name': name}
-        hotels_data = load_file(self.FILE)
+    def create_hotel(self, h_id, name):
+        """Function create a new hotel"""
+        new_hotel = {'hotel_id': h_id, 'hotel_name': name}
+        hotels_data = load_file(self.file)
         hotels_data.append(new_hotel)
-        save_file(self.FILE, hotels_data)
+        save_file(self.file, hotels_data)
 
 
     def delete_hotel(self, hotel_name):
-        hotels_data = load_file(self.FILE)
-        
+        """Function delete a hotel"""
+        hotels_data = load_file(self.file)
+
         for x in hotels_data:
             hotel_exists = any(x['name'] == hotel_name for hotel in hotels_data)
 
             if hotel_exists:
-               if x['name'] == hotel_name:
+                if x['name'] == hotel_name:
                     hotels_data.pop(x)
             else:
                 print(f"Hotel {hotel_name} does not exist.")
 
-        save_file(self.FILE, hotels_data)
+        save_file(self.file, hotels_data)
 
     def hotel_info(self, hotel_name):
-        hotels_data = load_file(self.FILE)
-        
+        """Function to print the information from a hotel"""
+        hotels_data = load_file(self.file)
+
         for x in hotels_data:
             hotel_exists = any(x['name'] == hotel_name for hotel in hotels_data)
 
             if hotel_exists:
-               if x['name'] == hotel_name:
+                if x['name'] == hotel_name:
                     print(x)
             else:
                 print(f"Hotel {hotel_name} does not exist.")
 
-        save_file(self.FILE, hotels_data)
-        
 
     def modify_hotel(self, hotel_id, new_hotel_name):
-        hotels_data = load_file(self.FILE)
-        
+        """Function modify a hotel's data"""
+        hotels_data = load_file(self.file)
+
         for x in hotels_data:
             hotel_exists = any(x['id'] == hotel_id for hotel in hotels_data)
 
             if hotel_exists:
-               if x['id'] == hotel_id:
+                if x['id'] == hotel_id:
                     hotels_data.update({'name': new_hotel_name})
             else:
                 print(f"Hotel {hotel_id} does not exist.")
 
-        save_file(self.FILE, hotels_data)
+        save_file(self.file, hotels_data)
 
 
-#Customer Classes 
+#Customer Classes
 class Customer:
-    
+    """Class that handles the methods needed for the customers"""
     def __init__(self, customer_id, customer_name, customer_email):
         self.customer_id = customer_id
         self.customer_name = customer_name
         self.email = customer_email
 
     def to_dictionary(self):
+        """Function to return its own dictionary"""
         return self.__dict__
 
 class ManageCustomer:
-
+    """Class that handles the management of the customers data"""
     def __init__(self, f):
-        self.FILE = f
+        self.file = f
 
-    def create_customer(self, id, name, email):
-        new_cust = {'customer_id': id, 'customer_name': name, 'email': email}
-        cust_data = load_file(self.FILE)
+    def create_customer(self, c_id, name, email):
+        """Function create a new customer"""
+        new_cust = {'customer_id': c_id, 'customer_name': name, 'email': email}
+        cust_data = load_file(self.file)
         cust_data.append(new_cust)
-        save_file(self.FILE, cust_data)
+        save_file(self.file, cust_data)
 
     def delete_customer(self, cust_id):
-        cust_data = load_file(self.FILE)
-        
+        """Function to delete a customer"""
+        cust_data = load_file(self.file)
+
         for x in cust_data:
             cust_exists = any(x['customer_id'] == cust_id for customer in cust_data)
 
             if cust_exists:
-               if x['customer_id'] == cust_id:
+                if x['customer_id'] == cust_id:
                     cust_data.pop(x)
             else:
                 print(f"Customer {cust_id} does not exist.")
 
-        save_file(self.FILE, cust_data)
+        save_file(self.file, cust_data)
 
     def customer_info(self, cust_id):
-        cust_data = load_file(self.FILE)
-        
+        """Function to print a customer's data"""
+        cust_data = load_file(self.file)
+
         for x in cust_data:
             cust_exists = any(x['customer_id'] == cust_id for customer in cust_data)
 
             if cust_exists:
-               if x['customer_id'] == cust_id:
+                if x['customer_id'] == cust_id:
                     print(x)
             else:
                 print(f"Customer {cust_id} does not exist.")
 
-        save_file(self.FILE, cust_data)
 
     def modify_customer(self, cust_id, new_name, new_email):
-        cust_data = load_file(self.FILE)
-        
+        """Function to modify a customer's data"""
+        cust_data = load_file(self.file)
+
         for x in cust_data:
             cust_exists = any(x['customer_id'] == cust_id for customer in cust_data)
 
             if cust_exists:
-               if x['customer_id'] == cust_id:
+                if x['customer_id'] == cust_id:
                     cust_data.cust_data.update({'customer_name': new_name}, {'email': new_email})
             else:
                 print(f"Customer {cust_id} does not exist.")
 
-        save_file(self.FILE, cust_data)
+        save_file(self.file, cust_data)
 
 
-#Reservation Classes 
+#Reservation Classes
 class Reservation(ABC):
-
+    """Abstract class for reservations"""
     @abstractmethod
     def to_dictionary(self):
-        pass
+        """Function to return its own dictionary"""
 
 
 class RoomReservation(Reservation):
-
+    """Class that handles the methods needed for a room reservation"""
     def __init__(self, reservation_id, room_num, customer_id, hotel_id):
         self.reservation_id = reservation_id
         self.room_num = room_num
@@ -174,32 +183,35 @@ class RoomReservation(Reservation):
 
     def to_dictionary(self):
         return self.__dict__
-    
+
 
 class ManageReservation:
-
+    """Class that handles the mmanagement of the reservations data"""
     def __init__(self, f):
-        self.FILE = f
+        self.file = f
 
     def new_reservation(self, resrv_id, room, cust_id, hotel_id):
-        new_resrv = {'reservation_id': resrv_id, 'room_num': room, 'customer_id': cust_id, 'hotel_id': hotel_id}
-        resrv_data = load_file(self.FILE)
+        """Function to create a new reservation"""
+        new_resrv = {'reservation_id': resrv_id, 'room_num': room,
+                     'customer_id': cust_id, 'hotel_id': hotel_id}
+        resrv_data = load_file(self.file)
         resrv_data.append(new_resrv)
-        save_file(self.FILE, resrv_data)
+        save_file(self.file, resrv_data)
 
     def cancel_reservation(self, resrv_id):
-        resrv_data = load_file(self.FILE)
-        
+        """Function to cancel a reservation, in this case it is deleted"""
+        resrv_data = load_file(self.file)
+
         for x in resrv_data:
             resrv_exists = any(x['reservation_id'] == resrv_id for reservation in resrv_data)
 
             if resrv_exists:
-               if x['reservation_id'] == resrv_id:
+                if x['reservation_id'] == resrv_id:
                     resrv_data.pop(x)
             else:
                 print(f"Reservation {resrv_id} does not exist.")
 
-        save_file(self.FILE, resrv_data)
+        save_file(self.file, resrv_data)
 
 
 
@@ -213,7 +225,3 @@ input_file = parser.parse_args()
 hotel_admin = ManageHotel(input_file.hotel_file)
 reserv_admin = ManageReservation(input_file.reserv_file)
 cust_admin = ManageCustomer(input_file.customer_file)
-
-
-
-
